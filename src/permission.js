4dd9bdbcd69fdbadf489a85240f739f2c0caf34e
Zeyan <liuzeyan@scantist.com>
2018-11-19 10:36:03 +0800

    FRONT-1 Turn of permission checking

--- src/permission.js
+++ src/permission.js
@@ -9,9 +9,10 @@ NProgress.configure({ showSpinner: false })// NProgress Configuration
 
 // permission judge function
 function hasPermission(roles, permissionRoles) {
-  if (roles.indexOf('admin') >= 0) return true // admin permission passed directly
-  if (!permissionRoles) return true
-  return roles.some(role => permissionRoles.indexOf(role) >= 0)
+  // if (roles.indexOf('admin') >= 0) return true // admin permission passed directly
+  // if (!permissionRoles) return true
+  // return roles.some(role => permissionRoles.indexOf(role) >= 0)
+  return true
 }
 
 const whiteList = ['/login', '/auth-redirect']// no redirect whitelist
